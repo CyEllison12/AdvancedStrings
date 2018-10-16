41910c53c578d5afb0b68f743daf119e1aa25395
Cy Ellison <dragonz2626@gmail.com>
2018-10-16 13:18:51 -0600

    added basic responseList

--- src/model/ChatBot.java
+++ src/model/ChatBot.java
@@ -1,10 +1,37 @@
 package model;
+import java.util.ArrayList; 
+
 
 public class ChatBot
 {
-	private ArrayList<String> responceList;
-	private String 
+	private ArrayList<String> responseList;
+	private ArrayList<String> spookyList;
 	
+	private String content;
+	private String joke;
+	private String currentuser;
+	
+	public ChatBot()
+	{
+		this.joke = "Whats a pirates favorite letter?";
+		this.content = new String("boring content");
+		this.currentuser = new String("This is default user");
+		
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList<String>();
+		
+		buildTheLists();
+	}
+
+	private void buildTheLists()
+	{
+		responseList.add("Hello! How are you?");
+		responseList.add("Goodbye - no more talking!");
+		responseList.add("Thats not nice!");
+		responseList.add("I don't understand...");
+		responseList.add("Thats pretty funny!");
+		responseList.add("You already said that!");
+	}
 	
 
 }
