5acc44774c493182c5e16113a3f46bc3e2e0f1b6
Cy Ellison <dragonz2626@gmail.com>
2019-04-23 13:56:07 -0600

    added chattwitter starting

--- src/chat/model/chatTwitter.java
+++ src/chat/model/chatTwitter.java
@@ -1,6 +1,18 @@
 package chat.model;
 
+import chat.controller.ChatController;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
 public class chatTwitter
 {
+	private ChatController app;
+	private Twitter chatTwitter;
+	
+	public chatTwitter(ChatController app)
+	{
+		this.app = app;
+		this.chatTwitter = TwitterFactory.getSingleton();
+	}
 
 }
